beace085e6ae7bbd03dc811ef821befa93032012
akashnb0 <akashajay510@gmail.com>
2023-03-31 11:59:38 +0530

    mag

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <header className='App-header'>
+    <h1>
+      Let we see the output of JAVASCRIPT
+    </h1>
+    <button onClick={window['alertMsg']}>Click Me to Call The Func</button>
+
+   </header>
   );
 }
 
